da2bda2be22a9dc514f42fb115b2ce9d0d108d06
Saurabh5151-123 <saurabhberad58@gmail.com>
2026-04-04 11:13:48 +0000

    commit pattern printing

--- Negative_and_Positive_numbers/Negative_and_Positive_numbers.py
+++ Negative_and_Positive_numbers/Negative_and_Positive_numbers.py
@@ -12,5 +12,13 @@ print("Negative Numbers",neg)
 
 # COMMAND ----------
 
-print("Hii")
-print("I am Saurabh")
+i=1
+j=1
+print("Right angle triangle * pattern using while loop")
+while i<=5:
+    j=1
+    while j<=i:
+        print("*",end=" ")
+        j=j+1
+    i=i+1
+    print(" ")
